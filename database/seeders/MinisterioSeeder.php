248cc10c39d803ea4ee457eaeefe029c484d53e4
MajoCastro <mariajosecastromamani@gmail.com>
2025-02-28 21:57:49 -0400

    hice cambios en seeders multa incremento y tipo 1 de lideres

--- database/seeders/MinisterioSeeder.php
+++ database/seeders/MinisterioSeeder.php
@@ -13,20 +13,20 @@ class MinisterioSeeder extends Seeder
     public function run(): void
     {
         $ministerios = [
-            ['nombre' => 'Ministerio de Adoración', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Niños', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Jóvenes', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Líderes', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Evangelismo', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Servicio y Voluntariado', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Ayuno', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Oración e Intercesión', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Alabanza y Adoración', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Visitas', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Ujieres', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Teatro y Danza', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Pandero', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
-            ['nombre' => 'Ministerio de Tecnología y Medios de Comunicación', 'logo' => null, 'multa_incremento' => 0, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Adoración', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Niños', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Jóvenes', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Líderes', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 1, 'estado' => 1],
+            ['nombre' => 'Ministerio de Evangelismo', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Servicio y Voluntariado', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Ayuno', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Oración e Intercesión', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Alabanza y Adoración', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Visitas', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Ujieres', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Teatro y Danza', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Pandero', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
+            ['nombre' => 'Ministerio de Tecnología y Medios de Comunicación', 'logo' => null, 'multa_incremento' => 2, 'tipo' => 0, 'estado' => 1],
         ];
 
         // Insertar en la base de datos
