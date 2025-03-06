bbda086006da1bb8d60511ec7d8c4f7a341a9f4b
MajoCastro <mariajosecastromamani@gmail.com>
2025-03-06 12:02:21 -0400

    cree el seeders de horarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             ConfiguracionSeeder::class,
             ActividadServicios::class,
             MinisterioSeeder::class,
+            Horarios::class
         ]);
     }
 }
