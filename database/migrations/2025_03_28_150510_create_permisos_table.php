a17311d89be80d988f228291da5465cd12bcfce3
MajoCastro <mariajosecastromamani@gmail.com>
2025-03-28 17:29:25 -0400

    cree la tabla migracion permisos, y permiso usuario

--- database/migrations/2025_03_28_150510_create_permisos_table.php
+++ database/migrations/2025_03_28_150510_create_permisos_table.php
@@ -14,16 +14,16 @@ class CreatePermisosTable extends Migration
     {
         Schema::create('permisos', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->comment('Creador de la excepción');
-            $table->date('fecha')->comment('Fecha de inicio de la excepción');
-            $table->date('hasta')->comment('Fecha de finalización de la excepción');
-            $table->boolean('dia_entero')->comment('Indica si la excepción cubre el día entero');
-            $table->time('hora_inicio')->nullable()->comment('Hora de inicio de la excepción');
-            $table->time('hora_fin')->nullable()->comment('Hora de fin de la excepción');
-            $table->string('motivo')->nullable()->comment('Motivo de la excepción');
+            $table->unsignedBigInteger('user_id')->comment('Creador del permiso');
+            $table->date('fecha')->comment('Fecha de inicio del permiso');
+            $table->date('hasta')->comment('Fecha de finalización del perimiso');
+            $table->boolean('dia_entero')->comment('Indica si el permiso cubre el día entero');
+            $table->time('hora_inicio')->nullable()->comment('Hora de inicio del permiso');
+            $table->time('hora_fin')->nullable()->comment('Hora de fin del permiso');
+            $table->string('motivo')->nullable()->comment('Motivo del permiso');
             $table->enum('estado', ['pendiente', 'autorizado', 'rechazado'])
                   ->default('pendiente')
-                  ->comment('Estado de la excepción');
+                  ->comment('Estado del permiso');
             $table->timestamps();
 
         });
---
+++ database/migrations/2025_03_28_171555_create_permiso_usuario_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permiso_usuario', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('users')->onDelete('cascade')->comment('Relación con la tabla users');
+                $table->foreignId('permiso_id')->constrained('permisos')->onDelete('cascade')->comment('Relación con la tabla permisos');
+                $table->timestamps();
+                $table->unique(['usuario_id', 'permiso_id']); // Garantizar combinaciones únicas
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permiso_usuario');
+    }
+};
