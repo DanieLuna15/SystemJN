b97c54d435c6c0de53030af41004667fa88801a7
Daniel Luna <danilunajose123@gmail.com>
2025-03-28 22:13:00 -0400

    Modificacion de migracion permisos

--- database/migrations/2025_03_28_150510_create_permisos_table.php
+++ database/migrations/2025_03_28_150510_create_permisos_table.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreatePermisosTable extends Migration
-
 {
     /**
      * Run the migrations.
@@ -16,16 +15,15 @@ class CreatePermisosTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id')->comment('Creador del permiso');
             $table->date('fecha')->comment('Fecha de inicio del permiso');
-            $table->date('hasta')->comment('Fecha de finalización del perimiso');
+            $table->date('hasta')->comment('Fecha de finalización del permiso');
             $table->boolean('dia_entero')->comment('Indica si el permiso cubre el día entero');
             $table->time('hora_inicio')->nullable()->comment('Hora de inicio del permiso');
             $table->time('hora_fin')->nullable()->comment('Hora de fin del permiso');
             $table->string('motivo')->nullable()->comment('Motivo del permiso');
-            $table->enum('estado', ['pendiente', 'autorizado', 'rechazado'])
-                  ->default('pendiente')
-                  ->comment('Estado del permiso');
+            $table->tinyInteger('estado')
+                  ->default(0)
+                  ->comment('Estado del permiso: 0 = Pendiente, 1 = Autorizado, 2 = Rechazado');
             $table->timestamps();
-
         });
     }
 
@@ -34,4 +32,3 @@ class CreatePermisosTable extends Migration
         Schema::dropIfExists('permisos');
     }
 }
-
